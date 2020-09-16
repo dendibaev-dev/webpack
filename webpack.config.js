02d1be75f8c5fa32474f20b9061f26d33c74f846
Arman Dendibaev <43402694+ndoto-dev@users.noreply.github.com>
2020-09-16 14:40:45 +0500

    created webpack config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === "production"
+const isDev = !isProd
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+module.exports = {
+    mode: "development",
+    context: path.resolve(__dirname, "src"),
+    entry: './index.js',
+    output: {
+        filename: filename("js"),
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        port: 8080,
+        hot: isDev,
+        writeToDisk: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd,
+            },
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    }
+};
